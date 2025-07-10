a1b9f437c9b1bb794af17f802faa4745e5bc2384
Fauzan <fauzanaldi52@gmail.com>
2025-07-10 16:51:15 +0700

    Fix for deploy with dynamic port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const authRoutes = require('./routes/auth');
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
