4a17d042b943e253e4f3470b5445f6f74cad56d1
Fauzan <fauzanaldi52@gmail.com>
2025-07-11 18:46:16 +0700

    Update DB config to use Supabase

--- config/database.js
+++ config/database.js
@@ -1,8 +1,15 @@
-const config = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Z@nss99_', 
-  database: 'sleep_monitoring',
-};
+require('dotenv').config();
+const { Pool } = require('pg');
 
-module.exports = config;
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+module.exports = pool;
