dcd4400c721b7bbb194775b3c2eb7ab371c9ebe7
Fauzan <fauzanaldi52@gmail.com>
2025-07-11 19:05:52 +0700

    Update config for Supabase connection

--- config/database.js
+++ config/database.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
-const { Pool } = require('pg');
 
-const pool = new Pool({
+module.exports = {
   host: process.env.PGHOST,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   database: process.env.PGDATABASE,
-  port: process.env.PGPORT,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
-module.exports = pool;
+  port: process.env.PGPORT || 5432,
+};
