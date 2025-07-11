5f4ed95004e62b30c8f386470081a2701789ec80
Fauzan <fauzanaldi52@gmail.com>
2025-07-12 01:04:03 +0700

    Update authController.js to fix Supabase connection

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcrypt');
-const User = require('/../models/user');
+const User = require('/models/user');
 
 const registerUser = async (req, res) => {
   const { email, password, username } = req.body;
